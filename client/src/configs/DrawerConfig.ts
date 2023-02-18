ce02a383edd6d42a98178e036e5599910373d5ef
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-02-18 16:14:19 +0900

    first commit

---
+++ client/src/configs/DrawerConfig.ts
@@ -0,0 +1,38 @@
+export const DrawerConfig = {
+  "pointRadius": { // 点の半径
+    "originalPoint": 1.5,
+    "spline": 1.5,
+    "dft": 1.5
+  },
+  "strokeWidth": { // 描線の太さ
+    "originalPath": 1.5,
+    "spline": 1.5,
+    "dft": 1.5
+  },
+  "colors": { // 描線の色
+    "originalPoint": "rgb(5, 0, 0)",
+    "originalPath": "rgb(0, 0, 0)",
+    "point": "rgb(0, 0, 25)",
+    "spline": "rgb(0, 0, 0)",
+    "dft": "rgb(2, 0, 0)",
+    "onion": {
+      "current": "rgba(30, 100, 100, 0.1)",
+      "others": "rgba(0, 100, 100, 0.05)"
+    }
+  },
+  drawer: {
+    type: "plain",
+    lines: {
+      color: "#C8C8C8",
+      padding: {
+        top: 70,
+        bottom: 10,
+        left: 50,
+        right: 50
+      },
+      strokeWidth: 2,
+      margin: 120
+    },
+    backgroundColor: "#eee"
+  }
+};
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Drawer, Figure, Stroke } from "@nkmr-lab/average-figure-drawer";
+import { DrawerConfig } from "configs/DrawerConfig";
+
+export const Home:React.FC =() => {
+
+// Object型として各Drawerを保持
+const drawers: any = {};
+
+window.onload = () => {
+  // Drawerの設定
+  drawers["drawer"] = new Drawer("#drawer", DrawerConfig);
+};
+const change = () => {
+  drawers["drawer"] = new Drawer("#drawer");
+}
+
+const undo = () => {
+  drawers["drawer"].undo();
+
+
+}
+
+  return (
+    <>
+      <h1>書けるよー</h1>
+      <div className="canvasWrapper">
+        <svg id="drawer" className="canvas write"></svg>
+      </div>
+      <button onClick={undo}>undo</button>
+      <button onClick={change}>button</button>
+    </>
+  );
+}
