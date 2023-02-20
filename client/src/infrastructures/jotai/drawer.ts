bb4b1c5d7d85aeed26723ff8cd63f170a970d9e7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-02-20 16:45:37 +0900

    fix

--- client/src/infrastructures/jotai/drawer.ts
+++ client/src/infrastructures/jotai/drawer.ts
@@ -4,6 +4,10 @@ import { atom, useAtom, useSetAtom } from 'jotai';
 @description
 drawerを保持
 **/
-export const drawerAtom: any = atom({});
+export const drawerAtom = atom<any>({});
 
+/**
+@description
+ストローク量を保持
+**/
 export const drawerNumOfStrokeAtom = atom<number>(0);
