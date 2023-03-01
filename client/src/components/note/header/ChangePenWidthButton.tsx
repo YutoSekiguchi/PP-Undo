72c7048cf9d2f84e243d11378f3a188cd47b238b
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-03-01 15:20:34 +0900

    ペンの太さの変更

--- client/src/components/note/header/ChangePenWidthButton.tsx
+++ client/src/components/note/header/ChangePenWidthButton.tsx
@@ -1,45 +1,35 @@
 import React, { useState } from "react";
 import { useAtom } from 'jotai'
 import { drawerAtom } from "@/infrastructures/jotai/drawer";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
 import Spacer from "@/components/common/Spacer";
 import { penWidthList } from "@/configs/PenWidthConfig";
 import { DrawerConfig } from "@/configs/DrawerConfig";
 
 export const ChangePenWidthButton: React.FC = () => {
-  // const [drawMode, setDrawMode] = useAtom(drawModeAtom);
   const [drawer, setDrawer] = useAtom(drawerAtom);
   const [isShowMenu, setIsShowMenu] = useState<boolean>(false);
   const widthList = penWidthList;
 
-  // const setPenMode = () => {
-  //   setDrawMode("pen");
-  // }
-
-  // const penIcon = (
-  //   <svg xmlns="http://www.w3.org/2000/svg" className="mode-svg" viewBox="0 0 24 24" stroke="currentColor" fill="none">
-  //     <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-  //   </svg>
-  // );
   const showMenu = () => {
     setIsShowMenu((prev) => !prev);
   }
 
   const changeWidth = (size: number) => {
     const newStrokeSize: number = Math.round((size) * 10)/10;
-    // let newDrawerConfig = DrawerConfig;
-    // newDrawerConfig.pointRadius = {
-    //   "originalPoint": newStrokeSize,
-    //   "spline": newStrokeSize,
-    //   "dft": newStrokeSize
-    // };
-    // newDrawerConfig.strokeWidth = {
-    //   "originalPath": newStrokeSize,
-    //   "spline": newStrokeSize,
-    //   "dft": newStrokeSize
-    // }
-    // drawer.setConfig(newDrawerConfig);
+    let newDrawerConfig = DrawerConfig;
+    newDrawerConfig.pointRadius = {
+      "originalPoint": newStrokeSize,
+      "spline": newStrokeSize,
+      "dft": newStrokeSize
+    };
+    newDrawerConfig.strokeWidth = {
+      "originalPath": newStrokeSize,
+      "spline": newStrokeSize,
+      "dft": newStrokeSize
+    }
+    drawer.setConfig(newDrawerConfig);
     drawer.setStrokeWidth(newStrokeSize);
     setDrawer(drawer);
   }
