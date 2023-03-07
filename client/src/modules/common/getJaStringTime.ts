c89cff5b8e93bafa65cee195be70a17a3d1a3e3c
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-03-07 13:30:00 +0900

    日本時間を指定した形で文字列として取得するmoduleの作成

---
+++ client/src/modules/common/getJaStringTime.ts
@@ -0,0 +1,31 @@
+const dates = ["year", "month", "day", "hour", "min", "sec"] as const;
+const finishString = ["/", "/", " ", ":", ":", ""];
+type DateType = typeof dates[number];
+
+export const getJaStringTime = (from: DateType = "year", until: DateType = "min") => {
+  const now = new Date();
+
+  const Year = now.getFullYear();
+  const Month = now.getMonth()+1;
+  const Day = now.getDate();
+  const Hour = now.getHours();
+  const Min = now.getMinutes();
+  const Sec = now.getSeconds();
+
+  const nowList: string[] = [String(Year), String(Month), String(Day), String(Hour), String(Min), String(Sec)];
+
+  const FromIndex = dates.indexOf(from);
+  const UntilIndex = dates.indexOf(until);
+
+  let res: string = "";
+  for(let i = FromIndex; i <= UntilIndex; i++) {
+    nowList[i] = nowList[i].length == 1
+      ? "0" + nowList[i]
+      : nowList[i]; 
+    res = i<UntilIndex
+      ? res + nowList[i] + finishString[i]
+      : res + nowList[i];
+  }
+
+  return res;
+}
