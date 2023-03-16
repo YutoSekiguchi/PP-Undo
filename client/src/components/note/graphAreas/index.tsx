9d25670ee070453f92b71aaf18180e53b0b32368
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-03-16 11:33:03 +0900

    remove comment out

--- client/src/components/note/graphAreas/index.tsx
+++ client/src/components/note/graphAreas/index.tsx
@@ -42,25 +42,6 @@ export const NoteGraphAreas: React.FC = () => {
       backgroundColor: "#eee"
     }
   });
-  // const StepperButton = styled(Button)({
-  //   color: "#37474f",
-  //   "&.Mui-disabled": {
-  //     color: "#37474f55",
-  //   }
-  // });
-  // const {buttonColor, isChoice, colorChange, index} = props;
-  // const [drawer, setDrawer] = useAtom(drawerAtom);
-
-  // const buttonStyle: {backgroundColor: string} = {
-  //   backgroundColor: `${buttonColor}`,
-  // }
-  
-  // const changeColor = (color: string) => {
-  //   colorChange(index);
-  //   drawer.setStrokeColor(color);
-  //   drawer.config.colors.originalPoint = color;
-  //   setDrawer(drawer);
-  // }
 
 	return (
     <Box className="graph-area">
