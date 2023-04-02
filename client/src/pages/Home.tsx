3149a24e28909efb3c843ecf9f5942cb690d376b
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-02 23:00:11 +0900

    fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Home:React.FC =() => {
   return (
     <>
-      <h1>書けるよー</h1>
+      <h1>PP-Undo</h1>
       <div className="canvasWrapper">
         <svg id="drawer" className="canvas write"></svg>
       </div>
