74c29f4753891d0e14a115d57732b1a00b567af1
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-02 22:59:00 +0900

    fix

--- client/src/@types/authentication.d.ts
+++ client/src/@types/authentication.d.ts
@@ -10,10 +10,10 @@ export interface PostUserType {
 }
 
 export interface UserDataType {
-  id: number;
-  name: string;
-  password: string;
-  gender: string;
-  age: number;
-  created_at: string;
+  ID: number;
+  Name: string;
+  Password: string;
+  Gender: string;
+  Age: number;
+  CreatedAt: string;
 }
