e0a39be91a315bf37a9ef31aaa474adc818574aa
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-05 09:32:13 +0900

    add type

--- backend/service/common.go
+++ backend/service/common.go
@@ -3,3 +3,12 @@ package service
 import "github.com/YutoSekiguchi/ppundo/model"
 
 type ExamUser model.ExamUser
+type NoteFolders model.NoteFolders
+type Notes model.Notes
+type Strokes model.Strokes
+type Logs model.Logs
+type WatchLogsCounts model.WatchLogsCounts
+type LogRedoCounts model.LogRedoCounts
+type UndoCounts model.UndoCounts
+type RedoCounts model.RedoCounts
+type PPUndoCounts model.PPUndoCounts
