64bb19a612958af8cc765edde650301303d3ec04
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-05 14:38:07 +0900

    add: api notes

---
+++ backend/controller/notes.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/YutoSekiguchi/ppundo/service"
+)
+
+
+// ノートを全て取得
+func (ctrl Controller) HandleGetAllNotes(c echo.Context) error {
+	var s service.NotesService
+	p, err := s.GetAllNotes(ctrl.Db)
+
+	return Res(c, p, err)
+}
+
+// idからノートの取得
+func (ctrl Controller) HandleGetNoteByID(c echo.Context) error {
+	var s service.NotesService
+	p, err := s.GetNoteByID(ctrl.Db, c)
+
+	return Res(c, p, err)
+}
+
+// nfidからノートを全て取得
+func (ctrl Controller) HandleGetNotesByNFID(c echo.Context) error {
+	var s service.NotesService
+	p, err := s.GetNotesByNFID(ctrl.Db, c)
+
+	return Res(c, p, err)
+}
+
+// ノートの追加
+func (ctrl Controller) HandlePostNote(c echo.Context) error {
+	var s service.NotesService
+	p, err := s.PostNote(ctrl.Db, c)
+
+	return Res(c, p, err)
+}
+
+// ノートの編集
+func (ctrl Controller) HandleUpdateNoteByID(c echo.Context) error {
+	var s service.NotesService
+	p, err := s.UpdateNoteByID(ctrl.Db, c)
+
+	return Res(c, p, err)
+}
---
+++ backend/service/notes.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type NotesService struct{}
+
+// GET
+// 全てのノートを取得
+func (s NotesService) GetAllNotes(db *gorm.DB) ([]Notes, error) {
+	var n []Notes
+	if err := db.Find(&n).Error; err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+// idからノートの取得
+func (s NotesService) GetNoteByID(db *gorm.DB, c echo.Context) (*Notes, error) {
+	n := new(Notes)
+	id := c.Param("id")
+
+	if err := db.Raw("SELECT * FROM `notes` WHERE id = ?", id).Scan(&n).Error; err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+// nfidからノートを全て取得
+func (s NotesService) GetNotesByNFID(db *gorm.DB, c echo.Context) ([]Notes, error) {
+	var n []Notes
+	nfid := c.Param("nfid")
+
+	if err := db.Raw("SELECT * FROM `notes` WHERE nfid = ?", nfid).Scan(&n).Error; err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+// POST
+// ノートの作成
+func (s NotesService) PostNote(db *gorm.DB, c echo.Context) (Notes, error) {
+	var notes Notes
+	c.Bind(&notes)
+
+	if err := db.Create(&notes).Error; err != nil {
+		return notes, err
+	}
+	return notes, nil
+}
+
+// PUT
+// ノートの編集
+func (s NotesService) UpdateNoteByID(db *gorm.DB, c echo.Context) (*Notes, error) {
+	var n *Notes
+	id := c.Param("id")
+	if err := db.Raw("SELECT * FROM `notes` WHERE id = ? LIMIT 1", id).Scan(&n).Error; err != nil {
+		return nil, err
+	}
+	if err := c.Bind(&n); err != nil {
+		return nil, err
+	}
+	db.Save(&n)
+
+	return n, nil
+}
