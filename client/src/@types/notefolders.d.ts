8804f2859ae69a3be5733a8776f661b63ac7a7e7
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-08 23:48:37 +0900

    typeの追加

--- client/src/@types/notefolders.d.ts
+++ client/src/@types/notefolders.d.ts
@@ -1,10 +1,21 @@
-export interface AddFolderDialogProps {
-  open: boolean;
-  closeDialog: () => void;
-}
+import React from "react";
 
 export interface PostNoteFolderType {
   UID: number;
   Name: string;
   ParentNFID: number;
+}
+
+export interface NoteFoldersDataType {
+  ID: number;
+  UID: number;
+  Name: string;
+  ParentNFID: number;
+  CreatedAt: string;
+}
+
+export interface AddFolderDialogProps {
+  open: boolean;
+  closeDialog: () => void;
+  setNoteFoldersData: React.Dispatch<React.SetStateAction<NoteFoldersDataType[]>>
 }
