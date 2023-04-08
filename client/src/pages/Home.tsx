36c5049ae475af1a746da84f49485c4ceffff195
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-08 14:53:13 +0900

    fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,16 @@
 import { userDataAtom } from "@/infrastructures/jotai/authentication";
 import { isAuth } from "@/modules/common/isAuth";
-import { Box } from "@mui/material";
 import { useAtom } from "jotai";
 import React, { useEffect, useState } from "react";
 
 export const Home:React.FC =() => {
-  
+  const [loginUserData, ] = useAtom(userDataAtom);
 
   return (
     <>
       {
-        isAuth()
+        isAuth() || loginUserData != null
         ? <>
-          <Box className="folder-list">
-            
-          </Box>
         </>
         : <h1>PP-Undo</h1>
       }
