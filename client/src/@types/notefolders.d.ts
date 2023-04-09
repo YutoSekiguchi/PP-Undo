4f5fb11819f1f5edffe1adb9c77f63bec9565069
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-09 15:14:07 +0900

    add: note type

--- client/src/@types/notefolders.d.ts
+++ client/src/@types/notefolders.d.ts
@@ -19,3 +19,49 @@ export interface AddFolderDialogProps {
   closeDialog: () => void;
   setNoteFoldersData: React.Dispatch<React.SetStateAction<NoteFoldersDataType[]>>
 }
+
+export interface PostNoteType {
+  NFID: number;
+  UID: number;
+  Title: string;
+  Width: number;
+  Height: number;
+  NoteImage: string;
+  StrokeData: string;
+  AvgPressure: number;
+  AvgPressureList: string;
+  AllAvgPressureList: string;
+  IsShowStrokeList: string;
+  AllStrokeCount: number;
+  StrokeCount: number;
+  UndoCount: number;
+  RedoCount: number;
+  LogRedoCount: number;
+  PPUndoCount: number;
+  SliderValue: number;
+  BackgroundImage: string;
+}
+
+export interface NoteDataType {
+  ID: number;
+  NFID: number;
+  UID: number;
+  Title: string;
+  Width: number;
+  Height: number;
+  NoteImage: string;
+  StrokeData: string;
+  AvgPressure: number;
+  AvgPressureList: string;
+  AllAvgPressureList: string;
+  IsShowStrokeList: string;
+  AllStrokeCount: number;
+  StrokeCount: number;
+  UndoCount: number;
+  RedoCount: number;
+  LogRedoCount: number;
+  PPUndoCount: number;
+  SliderValue: number;
+  BackgroundImage: string;
+  CreatedAt: string;
+}
