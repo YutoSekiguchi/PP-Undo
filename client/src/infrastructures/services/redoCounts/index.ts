8b97e36c34dc777a53b866650e71c6e4ac758db6
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-18 17:54:40 +0900

    redo情報をAPIに送信

---
+++ client/src/infrastructures/services/redoCounts/index.ts
@@ -0,0 +1,19 @@
+import { PostRedoCountDataType } from "@/types/note";
+import { API_URL } from "@/configs/settings";
+import axios from "axios";
+
+// Redoカウント
+export const addRedoCount = async (data: PostRedoCountDataType) => {
+  const url = `${API_URL}/redocounts`;
+  try {
+    const res = await axios.post(url, data);
+    if (res.status == 200) {
+      return res.data;
+    } else {
+      console.log(res);
+      return null;
+    }
+  } catch (error) {
+    throw(error);
+  }
+}
