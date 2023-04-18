2544bc986b8f4b4854e8f6cd9489d70214cd73c8
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-18 12:32:39 +0900

    undoCountを追加するserviceの追加

---
+++ client/src/infrastructures/services/undoCounts/index.ts
@@ -0,0 +1,19 @@
+import { PostUndoCountDataType } from "@/@types/note";
+import { API_URL } from "@/configs/settings";
+import axios from "axios";
+
+// Undoカウント
+export const addUndoCount = async (data: PostUndoCountDataType) => {
+  const url = `${API_URL}/undocounts`;
+  try {
+    const res = await axios.post(url, data);
+    if (res.status == 200) {
+      return res.data;
+    } else {
+      console.log(res);
+      return null;
+    }
+  } catch (error) {
+    throw(error);
+  }
+}
