3657cf52e757fadc39b487a8d821ffa5b9a14420
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-20 10:43:42 +0900

    fix

--- backend/model/client_logs.go
+++ backend/model/client_logs.go
@@ -5,7 +5,7 @@ import "time"
 type ClientLogs struct {
 	ID                       int       `gorm:"primary_key;not null;autoIncrement:true"`
 	NID                      int       `gorm:"not null;column:nid"`
-	Data                     StrokeData    `gorm:"not null;column:stroke_data"`
+	Data                     StrokeData    `gorm:"not null;column:data"`
 	CreatedAt                time.Time `sql:"DEFALUT:current_timestamp;column:created_at"`
 }
 
