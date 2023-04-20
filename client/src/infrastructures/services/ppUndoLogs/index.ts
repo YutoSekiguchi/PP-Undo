17a3bf17b0440e070444129601dee4f7e95ecfb5
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-20 10:32:15 +0900

    add: logに関するapi通信

---
+++ client/src/infrastructures/services/ppUndoLogs/index.ts
@@ -0,0 +1,35 @@
+import { PostClientLogDataType, PostLogDataType } from "@/@types/note";
+import { API_URL } from "@/configs/settings";
+import axios from "axios";
+
+// ログの追加
+export const addLog = async (data: PostLogDataType) => {
+  const url = `${API_URL}/logs`;
+  try {
+    const res = await axios.post(url, data);
+    if (res.status == 200) {
+      return res.data;
+    } else {
+      console.log(res);
+      return null;
+    }
+  } catch (error) {
+    throw(error)
+  }
+}
+
+// クライアント用のLogを追加
+export const addClientLog = async (data: PostClientLogDataType) => {
+  const url = `${API_URL}/clientlogs`;
+  try {
+    const res = await axios.post(url, data);
+    if (res.status == 200) {
+      return res.data;
+    } else {
+      console.log(res);
+      return null;
+    }
+  } catch (error) {
+    throw(error);
+  }
+}
