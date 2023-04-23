49aacbbf84351179cb2856c35c3dbc6bece258b4
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-23 14:57:47 +0900

    表示されているストローク数取得moduleの追加

--- client/src/modules/note/CalcIsShowStroke.ts
+++ client/src/modules/note/CalcIsShowStroke.ts
@@ -11,3 +11,15 @@ export const calcIsShowStrokeList = (strokes: any[]) => {
 
   return tmp;
 }
+
+export const calcIsShowStrokeCount = (strokes: any[]) => {
+  let tmp: number = 0;
+  strokes.map((stroke, i) => {
+    const color: string = stroke.color;
+    if (!(color.length == 9 && color.endsWith('00'))) {
+      tmp += 1;
+    }
+  })
+
+  return tmp;
+}
