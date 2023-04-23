1df4d37b1ea75e89e4303ef5dec12ea4fcc6416f
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-04-23 17:15:44 +0900

    fix: model name

--- backend/model/ppundo_counts.go
+++ backend/model/ppundo_counts.go
@@ -6,8 +6,8 @@ type PPUndoCounts struct {
 	ID                      int       `gorm:"primary_key;not null;autoIncrement:true"`
 	UID                     int       `gorm:"not null;column:uid"`
 	NID                     int       `gorm:"not null;column:nid"`
-	BeforePPUndoStrokeData  StrokeData    `gorm:"not null;column:before_ppundo_stroke_data"`
-	BeforePPUndoImageData   string    `gorm:"not null;column:before_ppundo_image_data"`
+	AfterPPUndoStrokeData  StrokeData    `gorm:"not null;column:after_ppundo_stroke_data"`
+	AfterPPUndoImageData   string    `gorm:"not null;column:after_ppundo_image_data"`
 	BeforePPUndoStrokeCount int       `gorm:"not null;column:before_ppundo_stroke_count"`
 	AfterPPUndoStrokeCount  int       `gorm:"not null;column:after_ppundo_stroke_count"`
 	CreatedAt               time.Time `sql:"DEFALUT:current_timestamp;column:created_at"`
