9305dde0827b11525ba2e2ef5c996fcac6db0ee0
YutoSekiguchi <yuutosekiguchi@gmail.com>
2023-07-06 20:36:52 +0900

    add

--- client/src/configs/settings.ts
+++ client/src/configs/settings.ts
@@ -3,4 +3,4 @@ export const NOTE_WIDTH_RATIO = 0.75;
 export const PRESSURE_ROUND_VALUE = 10000;
 export const BORDER_STRONG_PRESSURE = 0.6;
 export const BORDER_WAVE_PRESSURE = 0.1;
-export const BORDER_WAVE_COUNT = 4;
+export const BORDER_WAVE_COUNT = 5;
